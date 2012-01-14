c3c945982e2ce2e1e4365a6f376798399f0ee0ed
Gregory Brown <gregory.t.brown@gmail.com>
2012-01-14 15:14:56 -0500

    Pointer tests

--- test/tape_test.rb
+++ test/tape_test.rb
@@ -1,8 +1,24 @@
 require_relative "helper"
 
 describe "Tape" do
-  it "should initialize values to 0" do
+  it "must have an initial cell value of 0" do
     tape = TuringTarpit::Tape.new
     tape.cell_value.must_equal(0)
+  end
+
+  it "must have an initial pointer position of 0" do
+    tape = TuringTarpit::Tape.new
+    tape.position.must_equal(0)
+  end
+
+  it "must be able to increment the pointer position" do
+    tape = TuringTarpit::Tape.new
+
+    tape.increment_pointer
+
+    tape.position.must_equal(1)
+    
+    tape.increment_pointer
+    tape.position.must_equal(2)
   end
 end
