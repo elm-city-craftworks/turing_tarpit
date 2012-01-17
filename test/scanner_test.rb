11b00a088b69268938090f5e43f1bd2ef27aa08e
Gregory Brown <gregory.t.brown@gmail.com>
2012-01-16 19:32:36 -0500

    loop test

--- test/scanner_test.rb
+++ test/scanner_test.rb
@@ -17,4 +17,20 @@ describe "Scanner" do
     insist { scanner.next(true) }.must_raise(StopIteration)
   end
 
+  it "must be able to process a program with a forward jump" do
+    scanner = TuringTarpit::Scanner.new("[++++]-")
+    scanner.next(true).must_equal("-")
+    insist { scanner.next(true) }.must_raise(StopIteration)
+  end
+
+  it "must be able to process a program with a finite loop" do
+    scanner = TuringTarpit::Scanner.new("++[-]")
+    scanner.next(true).must_equal("+")
+    scanner.next(false).must_equal("+")
+    scanner.next(false).must_equal("-")
+    scanner.next(false).must_equal("-")
+
+    insist { scanner.next(true) }.must_raise(StopIteration)
+  end
+
 end
