d6494d1146825a3b01fe9533262633bab0c6e983
Renato Riccieri <renato.riccieri@gmail.com>
2012-01-20 01:56:57 -0200

    Split classes into separate files

---
+++ lib/interpreter.rb
@@ -0,0 +1,32 @@
+require_relative 'turing_tarpit'
+
+module TuringTarpit
+  class Interpreter
+    def initialize(tokenizer, tape)
+      @tokenizer = tokenizer
+      @tape    = tape
+    end
+
+    def run
+      loop do
+        case @tokenizer.next(@tape.cell_value)
+        when "+"
+          @tape.increment_cell_value
+        when "-"
+          @tape.decrement_cell_value
+        when ">"
+          @tape.increment_pointer
+        when "<"
+          @tape.decrement_pointer
+        when "."
+          putc(@tape.cell_value)
+        when ","
+          value = STDIN.getch.bytes.first
+          next if value.zero?
+
+          @tape.cell_value = value
+        end
+      end
+    end
+  end
+end
