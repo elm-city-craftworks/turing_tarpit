1c263bfba2240590143383195a7e35e11e2170dc
Gregory Brown <gregory.t.brown@gmail.com>
2012-01-24 22:13:08 -0500

    Two seconds is a small price to pay for peace of mind

--- test/suite.rb
+++ test/suite.rb
@@ -3,7 +3,4 @@ SimpleCov.start
 
 require_relative "units/interpreter_test"
 require_relative "units/evaluator_test"
-
-if ENV["TEST_ALL"]
-  require_relative "integration/evaluator_test"
-end
+require_relative "integration/evaluator_test"
