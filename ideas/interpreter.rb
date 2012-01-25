050fc7f3ba48e5f5ce86bf910e6658cc25b25545
Gregory Brown <gregory.t.brown@gmail.com>
2012-01-24 21:40:48 -0500

    Add an interpreter executable

--- ideas/interpreter.rb
+++
@@ -1,8 +0,0 @@
-require_relative "lib/turing_tarpit"
-
-tape    = TuringTarpit::Tape.new
-tokenizer = TuringTarpit::Tokenizer.new(ARGF.read)
-
-interpreter = TuringTarpit::Interpreter.new(tokenizer, tape)
-
-interpreter.run
--- ideas/tape_example.rb
+++
@@ -1,8 +0,0 @@
-tape = TuringTarpit::Tape.new
-
-tape.cell_value #=> 0
-tape.cell_value = 100 #=> 100
-tape.increment_cell_value #=> 101
-tape.decrement_cell_value #=> 100
-tape.increment_pointer
-tape.decrement_pointer
